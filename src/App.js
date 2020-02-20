a75aac40df83c1cad84a46eeae337573bd853b09
Marc <marcllopis89@gmail.com>
2020-02-20 10:20:21 +0100

    Adds todos categories and filters

--- src/App.js
+++ src/App.js
@@ -4,43 +4,98 @@ import List from './List';
 
 class App extends Component {
   state = {
-    singleToDo: '',
-    listOfToDos: []
+    singleToDo: '', // it will store the value of a single todo written in the input
+    todoChosenCategory: 'Coding', // it will store the value of whatever option selected on the select tag, by default is coding
+    noToDos: true, // used to show a text when there is still no todos written
+    filteredCategory: 'All', // used to show by default all options on filtering
+    listOfToDos: [] // an array that will contain each todo object
   }
 
-  onChange = (event) => {
+  onButtonChange = (event) => {
+    // grab the value of what the user wrote on the input and store it on singleTodo
     this.setState({ singleToDo: event.target.value });
   }
 
+  handleCategory = event => {
+    // grab the value of what the user chose on the select and store it on the state
+    this.setState({ todoChosenCategory: event.target.value });
+  }
+
   onSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // prevent the default behaviour of the form
+    // creates an object that will contain the todo text and the chosen category
+    let todoWithCategory = { todo: this.state.singleToDo, category: this.state.todoChosenCategory }
+
     this.setState({
-      singleToDo: '',
-      listOfToDos: [...this.state.listOfToDos, this.state.singleToDo]
+      singleToDo: '', // reset the value
+      todoChosenCategory: 'Coding', // reset the value
+      filteredCategory: 'All', // reset the value
+      noToDos: false, // changes the value to show something different
+      listOfToDos: [...this.state.listOfToDos, todoWithCategory] // takes what was in the array before and adds the new item that is an object of the todo text and category
     });
   }
 
   deleteToDo = (event) => {
-    let copyToDos = [...this.state.listOfToDos]
-    copyToDos.splice(event.target.id, 1)
+    let copyToDos = [...this.state.listOfToDos] // copy the todos array
+    copyToDos.splice(event.target.id, 1) // takes the index of the todo we clicked and deletes it
     this.setState({
-      listOfToDos: copyToDos
+      listOfToDos: copyToDos // updates our array with the deleted one
+    })
+  }
+
+  selectFilter = event => {
+    // stores in the state the category the user wants to be filtered
+    this.setState({
+      filteredCategory: event.target.innerText,
     })
   }
 
   render() {
+
+    // the next variable will contain the result of filtering what is inside of the list of todos compared to the category the user has clicked
+    let filteredCategories = this.state.listOfToDos.filter(todo => todo.category === this.state.filteredCategory);
+
+    // next variable checks if the user has selected All category, if so, it will return the full array of todos, if not, it will return whatever was filtered before
+    let filteredToDos = this.state.filteredCategory === 'All' ? this.state.listOfToDos : filteredCategories
+
+
     return (
       <div>
         <form className="App" onSubmit={this.onSubmit}>
+        {/* Input tag that has it's value binded to the state */}
           <input
             value={this.state.singleToDo}
-            onChange={this.onChange}
+            onChange={this.onButtonChange}
             placeholder="Write your ToDo here..."
             className="input-todo"
           />
+          {/* Select tag that binds to the state whatever option the user chose */}
+          <select className="select-category" onChange={this.handleCategory}>
+            <option value="Coding">Coding</option>
+            <option value="Sports">Sports</option>
+            <option value="Food">Food</option>
+            <option value="Extra">Extra</option>
+          </select>
           <button className="button-todo">Add it</button>
         </form>
-        <List deleteToDo={this.deleteToDo} listOfToDos={this.state.listOfToDos} />
+        {/* Depends if there are todos already created or not, it shows something different */}
+        {this.state.noToDos
+          ? <h2 className="no-todos-text">Add some ToDo's...</h2>
+          :
+          <div className="flex-it">
+            <List deleteToDo={this.deleteToDo} listOfToDos={filteredToDos} />{/* It sends as props the todos already filtered */}
+            <div className="filter-container">
+              {/* Options to filter */}
+              <h1>Filter it</h1>
+              <hr />
+              <p onClick={this.selectFilter} className="filter-text">Coding</p>
+              <p onClick={this.selectFilter} className="filter-text">Sports</p>
+              <p onClick={this.selectFilter} className="filter-text">Food</p>
+              <p onClick={this.selectFilter} className="filter-text">Extra</p>
+              <p onClick={this.selectFilter} className="filter-text">All</p>
+            </div>
+          </div>
+        }
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
+// This component will always show the todos that are coming through props, there is no logic on how to filter here since it's already done in the parent component.
+// It will show a text if there's nothing filtered and the corresponding filtered todos if there are any
+
 const List = props => (
-  <div className="todos-container">
-    {
-      props.listOfToDos.map((toDo, i) =>
-        <div key={i} className="todo-text">
-           - {toDo}
-           <button
-            className="single-todo-button"
-            id={i}
-            onClick={props.deleteToDo}
+  props.listOfToDos.length !== 0
+    ? <div className="todos-container">
+      {
+        props.listOfToDos.map((toDo, i) =>
+          <div key={i} className="todo-text">
+            - {toDo.todo} | <span className="category-text">{toDo.category}</span>
+            <button
+              className="single-todo-button"
+              id={i}
+              onClick={props.deleteToDo}
             >
               Delete
             </button>
-        </div>
-      )
-    }
-  </div>
+          </div>
+        )
+      }
+    </div>
+    : <h2 className="no-category-text">Nothing here yet...</h2>
+
 );
 
 export default List;
